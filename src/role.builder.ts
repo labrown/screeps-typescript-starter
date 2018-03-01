55c428eb71e3d7a88beb7079efb845c968a05ef0
Lance A. Brown <lance@bearcircle.net>
2018-02-28 23:20:57 -0500

    Incorporate BonzAI Traveler

--- src/role.builder.ts
+++ src/role.builder.ts
@@ -10,17 +10,17 @@ export function run(creep: Creep) {
 	}
 
 	if(creep.memory.building) {
-		var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-		if(targets.length) {
-			if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-				creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+		let target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+		if(target) {
+			if(creep.build(target) == ERR_NOT_IN_RANGE) {
+				creep.travelTo(target, {visualizePathStyle: {stroke: '#ffffff'}} as TravelToOptions);
 			}
 		}
 	}
 	else {
 		var sources = creep.room.find(FIND_SOURCES);
 		if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-			creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+			creep.travelTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}} as TravelToOptions);
 		}
 	}
 };
