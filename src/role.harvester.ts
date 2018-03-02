fd7986a09cfc09ab59086bd248f97c20480cce43
Lance A. Brown <lance@bearcircle.net>
2018-03-01 23:47:49 -0500

    Refactor to use similar logic as other roles

--- src/role.harvester.ts
+++ src/role.harvester.ts
@@ -1,27 +1,34 @@
 import * as roleBuilder from "./role.builder";
 
 export function run(creep: Creep) {
-    if(creep.carry.energy < creep.carryCapacity) {
-        var sources = creep.room.find(FIND_SOURCES);
-        if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-            creep.travelTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}} as TravelToOptions);
-        }
-    }
-    else {
-        var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION ||
-                            structure.structureType == STRUCTURE_SPAWN ||
-                            structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-                }
+
+    if(creep.memory.loading && creep.carry.energy == 0) {
+		creep.memory.loading = false;
+		creep.say('🔄 harvest');
+	}
+	else if(!creep.memory.loading && creep.carry.energy == creep.carryCapacity) {
+		creep.memory.loading = true;
+		creep.say('🚧 loading');
+	}
+
+    if (creep.memory.loading) {
+        let target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_SPAWN ||
+                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+            }
         });
         if(target) {
             if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.travelTo(target, {visualizePathStyle: {stroke: '#ffffff'}} as TravelToOptions);
             }
-        } else {
-            roleBuilder.run(creep);                
         }
-        
+    }
+    else {
+        let sources = creep.room.find(FIND_SOURCES);
+        if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+            creep.travelTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}} as TravelToOptions);
+        }
     }
 };
