15405b8bc702569769675264d88bb60cbc6157ff
Lance A. Brown <lance@bearcircle.net>
2018-03-02 04:47:15 -0500

    Move builder and upgrader into new AI code

--- src/CreepRunner.ts
+++ src/CreepRunner.ts
@@ -19,14 +19,40 @@ export class CreepRunner {
                 creep.say('🚧 working');
             }
 
+
             if (creep.memory.working) {
                 switch (creep.memory.role) {
                     case "harvester": this.harvester_work(creep);
+                        break;
+                    case "builder": this.builder_work(creep);
+                        break;
+                    case "upgrader": this.upgrader_work(creep);
+                        break;
                 }
             }
             else {
                 switch (creep.memory.role) {
                     case "harvester": this.harvester_load(creep);
+                        break;
+                    case "builder": this.builder_load(creep);
+                        break;
+                    case "upgrader": this.upgrader_load(creep);
+                        break;
+                }
+            }
+
+            // Set down road if possible
+            let controller = this.room.room.controller as StructureController;
+            if (controller.my) {
+                let roads = _.filter(this.room.structures, function (s) {
+                    return (s.pos.x == creep.pos.x && s.pos.y == creep.pos.y)
+                });
+                let sites = _.filter(this.room.sites, function (s) {
+                    return (s.pos.x == creep.pos.x && s.pos.y == creep.pos.y)
+                });
+                if (roads.length == 0 && sites.length == 0) {
+                    console.log(`creating road at ${creep.pos.x},${creep.pos.y} in ${this.room.name}`);
+                    this.room.room.createConstructionSite(creep.pos, STRUCTURE_ROAD);
                 }
             }
         }
@@ -49,6 +75,37 @@ export class CreepRunner {
 
     harvester_load(creep: Creep) {
         let source: Source = Game.getObjectById(creep.memory.source_id) as Source;
+        if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+            creep.travelTo(source, { visualizePathStyle: { stroke: '#ffaa00' } } as TravelToOptions);
+        }
+    }
+
+    builder_work(creep: Creep) {
+        let target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+        if (target) {
+            if (creep.build(target) == ERR_NOT_IN_RANGE) {
+                creep.travelTo(target, { visualizePathStyle: { stroke: '#ffffff' } } as TravelToOptions);
+            }
+        }
+    }
+
+    builder_load(creep: Creep) {
+        this.loadFromSource(creep);
+    }
+
+    upgrader_work(creep: Creep) {
+        if (creep.upgradeController(creep.room.controller as StructureController) == ERR_NOT_IN_RANGE) {
+            creep.travelTo(creep.room.controller as StructureController,
+                { visualizePathStyle: { stroke: '#ffffff' } } as TravelToOptions);
+        }
+    }
+
+    upgrader_load(creep: Creep) {
+        this.loadFromSource(creep);
+    }
+
+    loadFromSource(creep: Creep) {
+        let source = creep.pos.findClosestByPath(FIND_SOURCES);
         if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
             creep.travelTo(source, { visualizePathStyle: { stroke: '#ffaa00' } } as TravelToOptions);
         }
--- src/RoomMgr.ts
+++ src/RoomMgr.ts
@@ -1,14 +1,27 @@
+interface CreepMemory { 
+    role?: string,
+    building?: boolean,
+    upgrading?: boolean,
+    loading?: boolean,
+    source_id?: string,
+    working?: boolean
+}
+
 import { CreepRunner } from "CreepRunner";
+import { TowerRunner } from "TowerRunner";
 
 export class RoomMgr {
+
     name: string;
     room: Room;
     sources: Array<Source>;
     sites: Array<ConstructionSite>;
+    structures: Array<Structure>;
     creeps: Array<Creep>;
     spawns: Array<StructureSpawn>;
-    runner: CreepRunner;
-    
+    creepRunner: CreepRunner;
+    towerRunner: TowerRunner;
+
     constructor(room_name: string) {
         // name of this room
         this.name = room_name;
@@ -22,6 +35,9 @@ export class RoomMgr {
         // Get list of construction sites
         this.sites = this.room.find(FIND_CONSTRUCTION_SITES);
 
+        // Get list of structures
+        this.structures = this.room.find(FIND_STRUCTURES);
+        
         // Creeps in this room
         this.creeps = this.room.find(FIND_MY_CREEPS);
 
@@ -31,21 +47,60 @@ export class RoomMgr {
         // for each source we need 1 harvester creep
         for (let source of this.sources) {
             let creep_name = "harvester_" + source.id;
-            let creeps = _.filter(this.creeps, function(creep) {
+            let creeps = _.filter(this.creeps, function (creep) {
                 return creep.name == creep_name;
             });
-            if (creeps.length == 0 ) {
-                console.log(`Spawning ${creep_name}`);
+            if (creeps.length == 0) {
 
                 for (let spawn of this.spawns) {
-                    spawn.spawnCreep([WORK,CARRY,MOVE], creep_name, 
-                        { memory: { role: "harvester", source_id: source.id } as CreepMemory });
+                    if (!spawn.spawning) {
+                        console.log(`Spawning ${creep_name}`);
+                        spawn.spawnCreep([WORK, CARRY, MOVE], creep_name,
+                            { memory: { role: "harvester", source_id: source.id } as CreepMemory });
+                    }
                 }
             }
         }
 
+        // Create a build if we need one
+        let builders = _.filter(this.creeps, function(creep) {
+            return creep.memory.role == 'builder'
+        })
+        let builders_wanted = Math.ceil(this.sites.length / 5);
+        if (this.sites.length > 0 && builders.length < builders_wanted) {
+            let creep_name = "builder_" + Game.time;
+            for (let spawn of this.spawns) {
+                if (!spawn.spawning) {
+                    console.log(`Spawning ${creep_name}`);
+                    spawn.spawnCreep([WORK, CARRY, MOVE], creep_name,
+                        { memory: { role: "builder" } as CreepMemory });
+                }
+            }
+        }
+
+        // Make an upgrader if needed
+        let upgraders = _.filter(this.creeps, function(creep) {
+            return creep.memory.role == 'upgrader'
+        })
+        if (upgraders.length == 0) {
+            let creep_name = "upgrader_" + Game.time;
+            for (let spawn of this.spawns) {
+                if (!spawn.spawning) {
+                    console.log(`Spawning ${creep_name}`);
+                    spawn.spawnCreep([WORK, CARRY, MOVE], creep_name,
+                        { memory: { role: "upgrader" } as CreepMemory });
+                }
+            }
+            
+        }
+
         // Run creeps
-        this.runner = new CreepRunner(this);
-        this.runner.runCreeps();
+        this.creepRunner = new CreepRunner(this);
+        this.creepRunner.runCreeps();
+
+        // Run towers
+        this.towerRunner = new TowerRunner(this);
+        this.towerRunner.runTowers();
+
     }
 }
